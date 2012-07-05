796b15330d7cab76e09f8c159eb917f435f8c3cf
Tamas Pal <folti@balabit.hu>
2012-07-05 16:12:35 +0200

    now builds locally under ZWA on windows with mingw

--- autogen.sh
+++ autogen.sh
@@ -5,4 +5,6 @@ aclocal -I m4
 autoheader
 automake -a -c -f
 autoconf --force -i
+# fix old and broken libtools, which can't recognise 64 bit windows libs and dlls.
+find -name libtool -o -name ltmain.sh | xargs sed -i -e "s,'file format pe-i386.*\?','file format \(pei\*-i386\(\.\*architecture: i386\)\?|pe-arm-wince|pe-x86-64\)'," -e 's,cmd \/\/c,,'
 exit 0
